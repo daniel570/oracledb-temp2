73f1e93026d694c5da2e383e50bf33378418a9bc
root <root@f5a12e543277>
2020-02-18 01:02:31 +0000

    init

--- oracle-db-operator/examples/database/configmaps/init.sql
+++
@@ -1,9 +0,0 @@
--- ORDS USER
-CREATE USER C##DBAPI_CDB_ADMIN IDENTIFIED BY "LetsDocker";
-GRANT SYSDBA TO C##DBAPI_CDB_ADMIN CONTAINER = ALL;
-GRANT connect  TO C##DBAPI_CDB_ADMIN CONTAINER = ALL;
-
--- CONNECTION TO CONNECTION MANAGER
-alter system  set remote_listener='oracle-db-connection-manager-service:1521' scope=both sid='*';
-alter system register;
-
